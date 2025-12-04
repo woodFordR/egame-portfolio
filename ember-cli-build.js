941b8942de9963c1d3cdc3f82cc55114e0546775
woodFordR <adam@kaewell.com>
2025-12-04 18:39:43 -0500

    configured embroider v4 import, updated browserslist in package.json, removed conflicting webpack config, fixed common es module mixup, added tailwind & postcss plugins

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,31 +1,41 @@
 'use strict';
 
+const CssImport = require('postcss-import');
+const TailwindPostCSS = require('@tailwindcss/postcss');
+const Autoprefixer = require('autoprefixer');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   const app = new EmberApp(defaults, {
-    emberData: {
-      deprecations: {
-        // New projects can safely leave this deprecation disabled.
-        // If upgrading, to opt-into the deprecated behavior, set this to true and then follow:
-        // https://deprecations.emberjs.com/id/ember-data-deprecate-store-extends-ember-object
-        // before upgrading to Ember Data 6.0
-        DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: CssImport,
+            options: {
+              path: ['node_modules'],
+            },
+          },
+          {
+            module: TailwindPostCSS,
+          },
+          {
+            module: Autoprefixer,
+          },
+        ],
       },
     },
-    // Add options here
   });
 
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
-    staticAddonTestSupportTrees: true,
-    staticAddonTrees: true,
-    staticEmberSource: true,
-    staticInvokables: true,
     skipBabel: [
       {
         package: 'qunit',
       },
     ],
+    staticEmberSource: true,
+    staticAddonTrees: true,
+    staticAddonTestSupportTrees: true,
   });
 };
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    join('app', 'components', '**', '*.hbs'),
+    join('app', 'templates', '**', '*.hbs'),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
