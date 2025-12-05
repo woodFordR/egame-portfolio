a98a481811c522741498a742edf09a1c86381fc3
woodFordR <adam@kaewell.com>
2025-12-04 20:06:17 -0500

    added some ember blueprints with routes tests pages included

---
+++ app/routes/actors.js
@@ -0,0 +1,3 @@
+import Route from '@ember/routing/route';
+
+export default class ActorsRoute extends Route {}
---
+++ tests/unit/routes/actors-test.js
@@ -0,0 +1,11 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'egame-portfolio/tests/helpers';
+
+module('Unit | Route | actors', function (hooks) {
+  setupTest(hooks);
+
+  test('it exists', function (assert) {
+    let route = this.owner.lookup('route:actors');
+    assert.ok(route);
+  });
+});
