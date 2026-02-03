1257fd36a645ad80abf437e13095571f1bef4db0
woodFordR <adam@kaewell.com>
2026-02-03 06:29:01 -0500

    testing component for empty and rendered stars

--- tests/integration/components/star-rating-test.js
+++ tests/integration/components/star-rating-test.js
@@ -11,11 +11,11 @@ module('Integration | Component | star-rating', function(hooks) {
     this.set('updateRating', () => { });
 
     await render(hbs`
-<StarRating
-@rating={{this.rating}}
-@onUpdate={{this.updateRating}}
-/>
-`);
+      <StarRating
+        @rating={{this.rating}}
+        @onUpdate={{this.updateRating}}
+      />
+    `);
 
     assert
       .dom('[data-test-rr="full-star"]')
@@ -37,6 +37,22 @@ module('Integration | Component | star-rating', function(hooks) {
         { count: 3 },
         'The right amount of empty stars is rendered after changing rating'
       );
+  });
+
+  test('Calls onUpdate with the correct value', async function(assert) {
+    this.set('rating', 2);
+    this.set('updateRating', (rating) => {
+      assert.step(`Updated to rating: ${rating}`);
+    });
+
+    await render(hbs`
+      <StarRating
+        @rating={{this.rating}}
+        @onUpdate={{this.updateRating}}
+      />
+    `);
+    await click('[data-test-rr="star-rating-button"]:nth-child(4)');
+    assert.verifySteps(['Updated to rating: 4']);
   });
 });
 
